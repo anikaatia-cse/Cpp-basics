3cf0a77cc6048e84f473c7a5c89aaca4789eb3a1
anikaatia-cse <anikaatia23@gmail.com>
2026-09-02 07:43:52 +0600

    Completed Lab Assignment 2 in C++ Language.

---
+++ Lab assignments/assignments_2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+// Write a C++ program that will print your name, your father and mother in three separate lines.
+
+int main()
+{
+    cout << "My name is Anika Atia" << endl;
+    cout << "My father's name is Muhamed Mamunul Islam" << endl;
+    cout << "My mother's name is Shamim Ara Begum" << endl;
+    return 0;
+}
