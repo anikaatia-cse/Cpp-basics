3be08298a76bcb7c35fcc769ac3751dea35657dc
anikaatia-cse <anikaatia23@gmail.com>
2026-09-03 23:06:35 +0600

    Completed Lab Assignments 3 in C++ Language.

---
+++ Lab assignments/assignments_3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+// Write a C++ program that will print the sum of two variables a and b, where a = 10 and b = 20. 
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+    int sum = a + b;
+    cout << "The sum of a and b is: " << sum << endl;
+    return 0;
+}
